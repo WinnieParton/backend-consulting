0061c997c920e9e46ada44ed535c25550dce32c2
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-02-02 12:12:19 +0100

    ajout url default

--- src/main/java/esgi/infra/controller/PersonController.java
+++ src/main/java/esgi/infra/controller/PersonController.java
@@ -1,5 +1,19 @@
 package esgi.infra.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/")
 public class PersonController {
     
+    @GetMapping
+    public ResponseEntity<?> home() {
+
+        return ResponseEntity.ok("Welcome successs");
+    }
 }
